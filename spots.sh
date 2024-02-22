f2b76a09c450dc660c1234c4eefd5cfef0a99e1a
Nick Gerner <nick.gerner@gmail.com>
2024-02-21 16:00:56 -0800

    adding some more configuration and more info from pota

--- spots.sh
+++ spots.sh
@@ -2,9 +2,10 @@
 
 set -eu -o pipefail
 
-SCRIPTNAME=$0
+SCRIPTPATH=$(realpath $0)
+SCRIPTNAME=$(basename $0)
 function printusage {
-    echo "usage $(basename $SCRIPTNAME) [<option> ...]"
+    echo "usage $SCRIPTNAME [<option> ...]"
     echo ""
     echo "options:"
     echo "    --start    start daemon (must come before other options)"
@@ -16,19 +17,23 @@ function printusage {
 }
 
 PIDFILE=/tmp/spots.pid
-set -- `getopt -n$0 -u -a --longoptions="host port source start stop" "h" "$@"`
+if ! options=`getopt -n$0 -u -a --longoptions="host:,port:,source:,start,stop,help" "h" "$@"`; then
+    printusage
+    exit 1
+fi
 
 host="localhost"
 port=6214
 period=30
 src=""
 
+eval set -- "$options"
 while [ $# -gt 0 ]; do
     case $1 in
         --start)
             shift
             echo "starting $SCRIPTNAME $@"
-            start-stop-daemon --start -p ${PIDFILE} -b -m -x $SCRIPTNAME -- `echo $@ | sed 's/ -- / /'`
+            start-stop-daemon --start -p ${PIDFILE} -b -m -x $SCRIPTPATH -- `echo $@ | sed 's/ -- / /'`
             exit $?
             ;;
         --stop)
@@ -36,12 +41,22 @@ while [ $# -gt 0 ]; do
             start-stop-daemon --stop -p ${PIDFILE}
             exit $?
             ;;
-        --host) host=$2;shift;;
-        --port) port=$2;shift;;
-        --source) src=$2;shift;;
+        --host)
+            shift;
+            host=$1;
+            shift;;
+        --port)
+            shift;
+            port=$1;
+            shift;;
+        --source)
+            shift
+            src=$1;
+            shift;;
         --help|-h)
             printusage
             exit 0
+            ;;
         --) shift;break;;
     esac
     shift
@@ -55,7 +70,7 @@ fi
 
 while true; do
     if [[ "$src" == "pota" ]]; then
-        curl "https://api.pota.app/spot" | jq -r '.[] | ["DX", .spotter, .frequency, .activator, .comments, (.spotTime+"Z" | fromdateiso8601 | strftime("%H%M %Y-%m-%d"))] | @tsv' | nc -N -w 5 $host $port > /dev/null
+        curl -s "https://api.pota.app/spot" | jq -r '.[] | ["DX", .spotter, .frequency, .activator, .name+" "+.comments, (.spotTime+"Z" | fromdateiso8601 | strftime("%H%M %Y-%m-%d")), .locationDesc] | @tsv' | nc -N -w 5 $host $port > /dev/null
     elif [[ "$src" == "hamqth" ]]; then
         curl 'https://www.hamqth.com/dxc_csv.php?limit=200&band=10m' | tr '^' '\t' | awk '{printf("DX\t%s\n", $0) }' | nc -N -w 5 $host $port > /dev/null
     else
